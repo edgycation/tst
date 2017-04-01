d31adf619612a4ee8f5131805daafa02b9125360
Peikkohi <peikkok@gmail.com>
2017-04-01 17:49:44 +0000

    funktio joka muuttaa listan htmliksi

--- tst.py
+++ tst.py
@@ -1,6 +1,8 @@
 from flask import Flask, send_from_directory
 app = Flask(__name__)
 
+def lista_htmliksi(lista):return "<UL>" + "".join("<LI>" + x for x in sorted(lista)) + "</UL>"
+
 @app.route("/")
 def oletussivu():
     return send_from_directory('templates', "haku.html")
@@ -11,4 +13,3 @@ def tyyli():
 
 if __name__ == "__main__":
     app.run()
-
